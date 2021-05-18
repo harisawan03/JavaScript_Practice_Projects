764b12b8a206aa9f1c2cec465cbccf1621e7ef12
Haris <harisawan03@gmail.com>
2021-05-18 18:50:09 -0500

    minor refactoring

--- ColorFlipper/setup/app.js
+++ ColorFlipper/setup/app.js
@@ -1,17 +1,9 @@
-// Variables
-
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-
 const button = document.getElementById('btn');
-
 const display = document.getElementById('display');
 
-let c;
-
-// Script
-
 function changeColor() {
-  c = Math.floor(Math.random() * colors.length);
+  let c = Math.floor(Math.random() * colors.length);
   document.body.style.backgroundColor = colors[c];
   updateDisplay();
 }
--- ColorFlipper/setup/hex.js
+++ ColorFlipper/setup/hex.js
@@ -1,35 +1,14 @@
-// Variables
-
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-
 const button = document.getElementById('btn');
-
 const display = document.getElementById('display');
-
-// let h1, h2, h3, h4, h5, h6;
-let h;
-
-let i;
 
 let setColor;
 
-// Script
-
 function randomColor() {
-  h1 = hex[Math.floor(Math.random() * hex.length)];
-  h2 = hex[Math.floor(Math.random() * hex.length)];
-  h3 = hex[Math.floor(Math.random() * hex.length)];
-  h4 = hex[Math.floor(Math.random() * hex.length)];
-  h5 = hex[Math.floor(Math.random() * hex.length)];
-  h6 = hex[Math.floor(Math.random() * hex.length)];
-
-  setColor = '#'.concat(h1,h2,h3,h4,h5,h6);
-
-  // for(i=0; i<hex.length; i++){
-  //   setColor = '';
-  //   h = Math.floor(Math.random() * hex.length);
-  //   setColor = setColor.concat(hex[h]);
-  // }
+  setColor = '#';
+  for(let i = 0; i < 6; i++){
+    setColor += hex[Math.floor(Math.random() * hex.length)];
+  }
 
   document.body.style.backgroundColor = setColor;
   updateDisplay();
