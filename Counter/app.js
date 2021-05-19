584d9c58a6b89a5be59e9c77e7fbd6e8be888ceb
Haris <harisawan03@gmail.com>
2021-05-18 20:55:59 -0500

    created variables for buttons

--- Counter/app.js
+++ Counter/app.js
@@ -1,5 +1,8 @@
 const counter = document.querySelectorAll('#value');
+const btn_decrease = document.getElementById('decrease');
+const btn_reset = document.getElementById('reset');
+const btn_increase = document.getElementById('increase');
 
 counter.forEach(
-  
+
 )
