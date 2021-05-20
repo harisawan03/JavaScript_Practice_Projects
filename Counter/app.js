6b27bd7dc7417dd75f32bf13c57d0d3cf0d9e108
Haris <harisawan03@gmail.com>
2021-05-19 19:23:02 -0500

    implement counter and color change

--- Counter/app.js
+++ Counter/app.js
@@ -1,8 +1,30 @@
 const counter = document.querySelectorAll('#value');
-const btn_decrease = document.getElementById('decrease');
-const btn_reset = document.getElementById('reset');
-const btn_increase = document.getElementById('increase');
+const btn_decrease = document.querySelector('#decrease');
+const btn_reset = document.querySelector('#reset');
+const btn_increase = document.querySelector('#increase');
 
-counter.forEach(
+btn_decrease.addEventListener('click', function(){
+  counter[0].textContent--;
+  handleColor();
+}, false);
 
-)
+btn_reset.addEventListener('click', function(){
+  counter[0].textContent = 0;
+  handleColor();
+}, false);
+
+btn_increase.addEventListener('click', function(){
+  counter[0].textContent++;
+  handleColor();
+}, false);
+
+function handleColor() {
+  if (counter[0].textContent < 0) {
+    counter[0].style.color = 'red';
+  } else if (counter[0].textContent > 0) {
+    counter[0].style.color = 'green';
+  } else {
+    counter[0].style.color = 'black';
+  }
+  console.log(counter[0].textContent);
+}
